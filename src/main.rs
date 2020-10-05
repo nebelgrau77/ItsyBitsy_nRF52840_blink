151a632e42b89b79fba2bdc52f52fb1395238751
nebelgrau <nebelgrau@gmx.com>
2020-10-05 19:36:27 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+#![no_main]
+#![no_std]
+
+use embedded_hal::digital::v2::OutputPin;
+
+use panic_halt as _;
+
+use nrf52840_hal as hal;
+use hal::pac::{CorePeripherals, Peripherals};
+use hal::prelude::*;
+use hal::gpio::Level;
+use hal::delay::Delay;
+
+//use nrf52840_hal::gpio::Level;
+//use nrf52840_hal::delay::Delay;
+//use nrf52840_hal::prelude::*;
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    
+    let p = Peripherals::take().unwrap();
+    let core = CorePeripherals::take().unwrap();
+
+    let port0 = hal::gpio::p0::Parts::new(p.P0);
+    let mut led = port0.p0_06.into_push_pull_output(Level::Low);
+    
+    let mut delay = Delay::new(core.SYST);
+    
+    loop {       
+        
+        led.set_high().unwrap();
+        delay.delay_ms(500_u32);              
+        led.set_low().unwrap();
+        delay.delay_ms(500_u32);
+    }
+    
+}
